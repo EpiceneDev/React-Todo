c0b859edbd574d3eecd0c4f8cafca8a24edad3ff
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-09-02 12:46:37 -0700

    App base built, TodoList built

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,14 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+const TodoList = () => {
+    return (
+        <div className="todo-list">
+            <h1>Hello todolist here</h1>
+        </div>
+        // <div>{this.state.message}</div>;
+        )
+}
+
+export default TodoList;
